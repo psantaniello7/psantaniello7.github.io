cd5837853c2f027a50e53efd6cde34befb4103bf
Paolo Francesco Santaniello <paolosantaniello7@gmail.com>
2021-05-04 14:43:37 -0700

    add ar exp
    adds project folder local hosting AR experience

--- demos/json_pass/scripts/ar-button.js
+++ demos/json_pass/scripts/ar-button.js
@@ -1,4 +1,4 @@
-const INNER_FRAME_URL = 'https://infovisioninc.8thwall.app/inner-iframe-tryon'
+const INNER_FRAME_URL = './ar/index.html'
 
 // User control elements for the iframe AR experience.
 const IFRAME_ID = 'my-iframe'  // iframe containing AR content.
